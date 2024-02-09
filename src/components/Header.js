d3bc3d1e3c9ec01b8ced0a83109bda0ee6af033c
Eddie-ee <115641900+Eddie-ee@users.noreply.github.com>
2024-02-09 10:56:55 +0200

    Initial setup for header, button and slide

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+export default function Header() {
+    return (
+        <h1>this is Header component</h1>
+    )
+}
---
+++ src/components/Slide.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+export default function Slide() {
+    return (
+        <div className="slide-div">
+            <h2>Slide component</h2>
+            <p>Slide discription</p>
+        </div>
+
+    )
+}
