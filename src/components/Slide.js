91d3304b47e5c103bd1e996f755bf64ed410844d
Eddie-ee <115641900+Eddie-ee@users.noreply.github.com>
2024-02-15 14:09:16 +0200

    get random trick function

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -3,16 +3,25 @@ import ReactDOM from "react-dom/client"
 import {tricksArray} from "./tricksData"
 
 export default function Slide() {
+    function getRandomTrick() {
+        let randomnum = Math.floor(Math.random() * tricksArray.length) + 1
+        let randomTrick = tricksArray[randomnum]
+        return randomTrick
+    }
+
     return (
         <div className="slide-div">
             <h2>Slide component</h2>
-            <p>{tricksArray}</p>
+            <p>{getRandomTrick()}</p>
 
         </div>
 
 
     )
 }
+
+
+
 
 
 function getRandomCard() {
--- src/components/tricksData.js
+++ src/components/tricksData.js
@@ -12,3 +12,4 @@ export const tricksArray = [
     "Double Leg Takedown",
 ];
 
+
