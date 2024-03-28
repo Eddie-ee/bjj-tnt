be9043b057bcaeb573788206c474c87d5801cb3f
Eddie-ee <115641900+Eddie-ee@users.noreply.github.com>
2024-03-28 14:44:47 +0200

    code changes

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client"
 import {tricksArray} from "./tricksData"
 
 export default function Slide() {
+
     function getRandomTrick() {
         let randomnum = Math.floor(Math.random() * tricksArray.length) + 1
         let randomTrick = tricksArray[randomnum]
@@ -13,19 +14,11 @@ export default function Slide() {
         <div className="slide-div">
             <h2>Slide component</h2>
             <p>{getRandomTrick()}</p>
-
         </div>
-
-
     )
 }
 
 
 
 
-
-function getRandomCard() {
-    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-    let randomNum = Math.floor(Math.random() * numbers.length) + 1
-    return randomNum
-}
+// do ONE, SMALL thing.
