c3d430d3733ce4beaf8c4b03292c81cf5a2b8944
Eddie-ee <115641900+Eddie-ee@users.noreply.github.com>
2024-05-16 08:27:38 +0200

    edits

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -19,10 +19,7 @@ export default function Slide() {
                 area={randomTip.name} 
                 tip={randomTip.description} 
                 />
-            {/* Old, potentially unneeded code?
-            <h2>Slide component</h2>
-            <p id="slide-el">{randomTip.name}</p>
-            <p>{randomTip.description}</p> */}
+            
         </div>
     );
 }
