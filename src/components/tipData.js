24ae288fc173185d129b31abf3ece2bdb002916f
Eddie-ee <115641900+Eddie-ee@users.noreply.github.com>
2024-06-11 14:08:58 +0200

    edits

--- src/components/tipData.js
+++ src/components/tipData.js
@@ -2,99 +2,120 @@ export const tipData = [
     { 
         name: 'Crossface', 
         description: 'Putting your hand across their neck or face, as deep as possible with elbow/bicep flush against opponent neck.',
-    },
-    { 
-        name: 'Crossface', 
-        description: 'Putting your hand across the',
+        level: 'White'
     },
     {
         name: 'Under Side Control',
-        description: 'When shrimping or creating space, make use of double movement (double shrimp, double bridge) to make more space & then reguard.'
+        description: 'When shrimping or creating space, make use of double movement (double shrimp, double bridge) to make more space & then reguard.',
+        level: 'White'
     },
     {
         name: 'Any',
-        description: 'Good Jiu Jitsu is more about small manipulations seting up effective attacks/defence (arm bars, triangles, escapes, etc.) than big, powerful moves - think physio Jaco.'
+        description: 'Good Jiu Jitsu is more about small manipulations seting up effective attacks/defence (arm bars, triangles, escapes, etc.) than big, powerful moves - think physio Jaco.',
+        level: 'Blue'
     },
     {
         name: 'In Closed Guard',
-        description: 'Generally, try create space between you and opponent & try get out of guard.'
+        description: 'Generally, try create space between you and opponent & try get out of guard.',
+        level: 'White'
     },
     {
         name: 'Under Top Mount',
-        description: 'Pull opponent neck/head close to you, limiting their space to operate.'
+        description: 'Pull opponent neck/head close to you, limiting their space to operate.',
+        level: 'White'
+        
     },
     {
         name: 'Under Top Mount',
-        description: 'It is better to be on side than back.'
+        description: 'It is better to be on side than back.',
+        level: 'White'
     },
     {
         name: 'Side Control',
-        description: 'Use your hand(s) to push opponent head both in attach & defence. Where the head goes, the body follows.'
+        description: 'Use your hand(s) to push opponent head both in attack & defence. Where the head goes, the body follows.',
+        level: 'Blue'
+
     },
     {
         name: 'Have Side Control',
-        description: 'Use chest-to-chest pressure, not over-leaning but chest on median line, & make the time count.'
+        description: 'Use chest-to-chest pressure, not over-leaning but chest on median-to-nearside line: camp & make the time count.',
+        level: 'White'
     },
     {
         name: 'Under Half Guard',
-        description: 'Stay on side & fight not to have far-side shoulder to the ground. Create space between you and opponent as much as you can.'
+        description: 'Stay on side & fight not to have far-side shoulder to the ground. Create space between you and opponent as much as you can.',
+        level: 'White'
     },
     { 
         name: 'Side Control', 
-        description: 'Pull opponent into you with your crossface and underhook arms', 
+        description: 'Pull opponent into you with your crossface and underhook arms: bicep-of-justice style!', 
+        level: 'Blue'
     },
     {
         name: 'Under Top Mount',
         description: 'Keep your elbows in, close to your body (ribs), hands on chest',
+        level: 'White'
     },
     {
         name: 'Under Top Mount',
         description: 'Keep knees up, in tight with heels close to but, then buck, bridge and roll. Remember to look up and point your eyes to where you are bridging to.',
+        level: 'White'
     },
     {
         name: 'Have Closed Guard',
-        description: 'When opponent is trying to break guard (especially big guys you cannot get your legs around) you can keep your legs flat & hook your feet on the insides of his legs & extend to create space to push him down & lift legs into an attack',
+        description: 'When opponent is trying to break guard (especially big guys you cannot get your legs around) you can keep your feet flat (behind him) & hook your feet on the insides of his legs & extend to create space to push him down & lift legs into an attack',
+        level: 'Blue'
     },
     {
         name: 'Any',
         description: 'Remember to breath: deep, consistent & through the nose.',
+        level: 'Blue'
     },
     {
         name: 'Have Closed Guard',
-        description: 'When doing arm drag, grab sameside wrist, opposing open hand shoots through deep (between grabbed arm and opponent chest) & hook onto outside of arm. Drag gripped arm while pushing opponent wrist underneath own arm toward floor.',
+        description: 'When doing arm drag, grab sameside wrist, opposing open hand shoots through deep (between grabbed arm and opponent chest) & hook onto outside of arm/tricep. Drag gripped arm while pushing opponent wrist underneath own arm toward floor.',
+        level: 'White'
     },
     {
         name: 'In/Under Side Control',
         description: 'Your Near Arm is the arm closest to your body & Far Arm is the arm that is farther away from your body',
+        level: 'White'
     },
     {
         name: 'Under Side Control',
-        description: 'If rolling with a more senior belt, keep what is normally your far arm & elbow (throat frame) close to your chest & hand on your chest.',
+        description: 'If rolling with a higher belt, keep what is normally your far arm & elbow (throat frame) close to your chest & hand on your chest.',
+        level: 'White'
     },
     {
         name: 'Any',
         description: 'Remember to breath: deep, consistent & through the nose.',
+        level: 'Blue'
     },
     {
         name: 'In Turtle',
-        description: 'Try protect your neck & prevent opponent connecting their hands beneath you'
+        description: 'Try protect your neck & prevent opponent connecting their hands beneath you',
+        level: 'White'
     },
     {
         name: 'Any',
         description: 'Prioritise position & control over moves & activity. Play smart & let them gas out.',
+        level: 'Blue'
     },
     {
         name: 'In Back Control (seated)',
         description: 'Try not to fall on the chocking arm (closest to your neck) side; instead fall toward oppisite are, trapping opponent arm.',
+        level: 'White'
     },
     {
         name: 'Any',
-        description: 'Generally, hands fight hands & feet fight feet.'
+        description: 'Generally, hands fight hands & feet fight feet.',
+        level: 'White'
     },
     {
         name: 'Have Back Control (seated)',
         description: 'Opponent will try to fall on the non-choking arm, trapping your arm. Use this as distraction while falling to side & cup defending arm down whilst lifting leg - get heel as close to defending arm elbow - to trap opponent arm.',
-    },
+        level: 'White'
+    },  
 
 ];
   
